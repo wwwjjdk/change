918f6e2bf2f20a27d879ccec163df0f8b94e1380
wwwjjdk <goshasavtch@yandex.ru>
2022-05-05 23:05:26 +0300

    second

--- src/main/java/Cacl.java
+++ src/main/java/Cacl.java
@@ -1,13 +1,14 @@
 public class Cacl {
-    public int monthlyPayment(int s, double r, int t){
+    public int monthlyPayment(int s, double r, int t) {
         // s*(r*(1+r)^t/(1+r)^t-1)
-        return 0;//(int)Math.floor(s*((r*Math.pow((1+r),t))/(Math.pow((1+r),t)-1)));
+        return (int) Math.floor(s * ((r * Math.pow((1 + r), t)) / (Math.pow((1 + r), t) - 1)));
     }
 
-    public int payOut(int mp, int t){
-        return  0;//mp*t;
+    public int payOut(int mp, int t) {
+        return mp * t;
     }
-    public int overpayment(int payOut, int s){
-        return 0;//payOut-s;
+
+    public int overpayment(int payOut, int s) {
+        return payOut - s;
     }
 }
